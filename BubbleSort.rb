fdbf8f5e16eb16e881cf32c63d81759cc3404d09
Sone Gillis <sone_gillis@yahoo.com>
2019-06-07 16:46:05 +0100

    Implement the bubble sort

--- BubbleSort.rb
+++ BubbleSort.rb
@@ -1,1 +1,15 @@
+list = [4,3,78,2,0,2]
+def bubble_sort(array)
+    for i in (0...array.length)
+        for j in (0...array.length-i-1)
+            if array[j] > array[j+1]
+                temp = array[j+1]
+                array[j+1] = array[j]
+                array[j] = temp
+            end
+        end
+    end
+    array
+end
 
+puts bubble_sort(list)

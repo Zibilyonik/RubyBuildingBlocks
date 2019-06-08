d85737d6d672150fecef259fd16b767deba5907e
Zibilyonik <34018257+Zibilyonik@users.noreply.github.com>
2019-06-08 18:11:49 +0200

    Code lint edited, making it easier on the eye

--- BubbleSort.rb
+++ BubbleSort.rb
@@ -1,29 +1,29 @@
 list = [4,3,78,2,0,2]
 list_words=["hi","hello","hey"]
 def bubble_sort(array)
-    for i in (0...array.length)
-        for j in (0...array.length-i-1)
-            if array[j] > array[j+1]
-                temp = array[j+1]
-                array[j+1] = array[j]
-                array[j] = temp
-            end
-        end
+  for i in (0...array.length)
+    for j in (0...array.length - i - 1)
+      if array[j] > array[j + 1]
+        temp = array[j + 1]
+        array[j + 1] = array[j]
+        array[j] = temp
+      end
     end
+  end
     array
 end
 def bubble_sort_by(array)
   for i in (0...array.length)
-    for j in (0...array.length-i-1)
-      result = yield array[j],array[j+1]
+    for j in (0...array.length - i - 1)
+      result = yield array[j],array[j + 1]
       if result > 0
-        temp=array[j+1]
-        array[j+1]=array[j]
-        array[j]=temp
+        temp = array[j + 1]
+        array[j + 1] = array[j]
+        array[j] = temp
       end
     end
   end
   array
 end
-puts bubble_sort_by(list_words) {|left,right| left.length-right.length}
+puts bubble_sort_by(list_words) {|left,right| left.length - right.length}
 puts bubble_sort(list)
